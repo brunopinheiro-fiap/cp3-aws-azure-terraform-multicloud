44149b1eb971ea5aa550f5dd45daa288500cb9ef
brunopinheiro-fiap <162511151+brunopinheiro-fiap@users.noreply.github.com>
2024-10-23 22:11:32 -0300

    PROVISIONAMENTO MULTICLOUD

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -70,7 +70,7 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "PublicIPAddress"
   location            = var.azure_location
   resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 # VNET20 (Privada)
